d11db17b8b5de3c8521b2e12e3c13cbc351bf203
Jules Bonnard <julesbonnard@gmail.com>
2025-12-08 10:58:35 +0100

    feat: ajouter le type Dtype pour la gestion des formats de données

--- env.d.ts
+++ env.d.ts
@@ -5,3 +5,19 @@ type Completion = {
   answer: string
   context: DocumentInterface
 }
+
+type Dtype =
+  | "auto"
+  | "fp32"
+  | "fp16"
+  | "q8"
+  | "int8"
+  | "uint8"
+  | "q4"
+  | "bnb4"
+  | "q4f16"
+  | Record<
+    string,
+    "auto" | "fp32" | "fp16" | "q8" | "int8" | "uint8" | "q4" | "bnb4" | "q4f16"
+  >
+  | undefined;
